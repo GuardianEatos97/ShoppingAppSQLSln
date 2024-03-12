44e3b9b4126fa9e63d746b7e765c4468576f8aae
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-03-12 15:05:20 +0200

    Add project files.

---
+++ ShoppingAppSQL/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingAppSQL
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShoppingAppSQL/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShoppingAppSQL
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
