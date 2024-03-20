c88d70bb87f449fcf76dddb1802b55a7417a91e5
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-03-20 02:32:59 +0200

    Remove button added

--- ShoppingAppSQL/Models/CartPage.xaml.cs
+++ ShoppingAppSQL/Models/CartPage.xaml.cs
@@ -1,9 +1,21 @@
+using ShoppingAppSQL.DataBaseItems;
 namespace ShoppingAppSQL.Models;
 
 public partial class CartPage : ContentPage
 {
-	public CartPage()
-	{
-		InitializeComponent();
-	}
+    private readonly CartViewModel _viewModel;
+
+    public CartPage()
+    {
+        InitializeComponent();
+        _viewModel = (CartViewModel)BindingContext;
+    }
+
+    private void OnRemoveClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is CartItem item)
+        {
+            _viewModel.RemoveItem(item);
+        }
+    }
 }
