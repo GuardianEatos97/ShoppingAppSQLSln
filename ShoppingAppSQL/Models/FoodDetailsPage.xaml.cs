5ea7df4d9ee9b775e26cab61e981748c673f2898
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-03-20 16:25:48 +0200

    UI Improvements

--- ShoppingAppSQL/Models/FoodDetailsPage.xaml.cs
+++ ShoppingAppSQL/Models/FoodDetailsPage.xaml.cs
@@ -4,9 +4,21 @@ namespace ShoppingAppSQL.Models;
 
 public partial class FoodDetailsPage : ContentPage
 {
-	public FoodDetailsPage(FoodItemViewModel item)
+    private readonly CartViewModel _viewmodel;
+    public FoodDetailsPage(FoodItemViewModel item)
 	{
 		InitializeComponent();
         BindingContext = item;
-	}
+        _viewmodel = (CartViewModel)BindingContext;
+    }
+
+  /*  private void AddToCartBtn_Clicked(object sender, EventArgs e)
+    {
+
+        if (sender is Button button && button.BindingContext is CartItem item)
+        {
+            _viewmodel.AddItem(item);
+        }
+    }*/
+        
 }
